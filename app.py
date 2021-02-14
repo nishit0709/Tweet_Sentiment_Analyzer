1ec7285c00ef91e319a776e56b8d37c013fad5a1
nishit0709 <60532223+nishit0709@users.noreply.github.com>
2021-02-14 18:50:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from opencage.geocoder import OpenCageGeocode
 import streamlit as st
 import os
 import tweepy as tw
@@ -7,22 +8,28 @@ api_key = 'EHQ0MqA1nubDwv3G0Q4HyRtOg'
 api_secret_key = 'vZFBEl7VDgMdTHKWR6QuIigJk7Ssd3qsYRH0a4goryLM7FaoCX'
 access_token = '1358825958392754178-0YbtWtO9wcEEULprlYeImaAxwFOZ6x'
 access_token_secret = 'gW6sWqrs10x6iBaewaoh9H4sSYIDJ9fCBGhB1zNJZYe1k'
+
 auth = tw.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
+
 word=st.text_input("Enter the search topic for the tweets: ")
+query = st.text_input("Enter the area you want to search: ")
+
 if word:
     search_words = "#"+word    
     date_since = "2021-02-01"
     tweets = tw.Cursor(api.search,geocode='28.644800,77.216721,50mi',q=search_words,lang="en",since=date_since).items(5)
     new_search = search_words + " -filter:retweets"
 
-from opencage.geocoder import OpenCageGeocode
+
 key='06e141dcc97045b18d9854d5a0cf3987'
 geocoder = OpenCageGeocode(key)	
 query = st.text_input("Enter the area you want to search: ")
+
 if query:
     rad=st.text_input("Enter the search radius in miles: ")
+    num=(int)st.text_input("Enter the number of tweets: ")
     if rad:
         results = geocoder.geocode(query)
         lat = results[0]['geometry']['lat']
@@ -31,7 +38,7 @@ if query:
         lng=str(lng)
         rad=str(rad)
         coord=lat+','+lng+','+rad+'mi'
-        tweets = tw.Cursor(api.search,geocode=coord,q=new_search,lang="en",since=date_since).items(5)
+        tweets = tw.Cursor(api.search,geocode=coord,q=new_search,lang="en",since=date_since).items(num)
 
         clean_tweets=[]
         clean_tweets=[tweet.text for tweet in tweets if 'RT @' not in tweet.text ]
@@ -45,8 +52,13 @@ if query:
                 positive_tweets.append(tweet)
             return positive_tweets, negative_tweets
         positive, negative = analyze()
-        st.write(positive , '\n\n', negative)
-        st.write(len(positive), ' VS  ', len(negative))
-
+        
+        st.write("Positive Tweets \n")
+        for tweet in positive
+        	st.write(tweet)
+        st.write("Negative Tweets \n")
+        for tweet in negative
+        	st.write(tweet)
+       st.write(len(positive), ' VS  ', len(negative))
 
 
