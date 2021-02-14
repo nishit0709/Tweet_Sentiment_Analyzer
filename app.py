5f39cd742a44d4323c33af87d82368a632002be6
nishit0709 <60532223+nishit0709@users.noreply.github.com>
2021-02-14 21:10:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
+from opencage.geocoder import OpenCageGeocode
 import streamlit as st
 import os
 import tweepy as tw
 import pandas as pd
 from textblob import TextBlob
+import plotly.graph_objects as go
+
 api_key = 'EHQ0MqA1nubDwv3G0Q4HyRtOg'
 api_secret_key = 'vZFBEl7VDgMdTHKWR6QuIigJk7Ssd3qsYRH0a4goryLM7FaoCX'
 access_token = '1358825958392754178-0YbtWtO9wcEEULprlYeImaAxwFOZ6x'
 access_token_secret = 'gW6sWqrs10x6iBaewaoh9H4sSYIDJ9fCBGhB1zNJZYe1k'
+key='06e141dcc97045b18d9854d5a0cf3987'
+
+
 auth = tw.OAuthHandler(api_key, api_secret_key)
 auth.set_access_token(access_token, access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
-word=st.text_input("Enter the search topic for the tweets: ")
-if word:
-    search_words = "#"+word    
-    date_since = "2021-02-01"
-    tweets = tw.Cursor(api.search,geocode='28.644800,77.216721,50mi',q=search_words,lang="en",since=date_since).items(5)
-    new_search = search_words + " -filter:retweets"
+geocoder = OpenCageGeocode(key)
 
-from opencage.geocoder import OpenCageGeocode
-key='06e141dcc97045b18d9854d5a0cf3987'
-geocoder = OpenCageGeocode(key)	
+st.title("Tweet Sentiment Analyzer")
+word=st.text_input("Enter the search topic for the tweets: ")
 query = st.text_input("Enter the area you want to search: ")
+search_words = "#"+word    
+date_since = "2021-02-01"
 if query:
     rad=st.text_input("Enter the search radius in miles: ")
     if rad:
@@ -31,7 +33,7 @@ if query:
         lng=str(lng)
         rad=str(rad)
         coord=lat+','+lng+','+rad+'mi'
-        tweets = tw.Cursor(api.search,geocode=coord,q=new_search,lang="en",since=date_since).items(5)
+        tweets = tw.Cursor(api.search,geocode=coord,q=search_words,lang="en",since=date_since).items(50)
 
         clean_tweets=[]
         clean_tweets=[tweet.text for tweet in tweets if 'RT @' not in tweet.text ]
@@ -45,8 +47,22 @@ if query:
                 positive_tweets.append(tweet)
             return positive_tweets, negative_tweets
         positive, negative = analyze()
-        st.write(positive , '\n\n', negative)
-        st.write(len(positive), ' VS  ', len(negative))
+        
+        st.header("Positive Tweets: \n")
+        i=1
+        for tweet in positive:
+            st.write(i,']   ',tweet)
+            i+=1
+        st.header("\n\nNegative Tweets: \n")
+        i=1
+        for tweet in negative:
+            st.write(i,']   ',tweet)
+            i+=1
 
-
-
+        st.header("\n\nComparison: \n")
+        pas=len(positive)
+        neg=len(negative)                
+        labels = ['Positive','Negative']
+        values = [pas,neg]
+        fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
+        st.plotly_chart(fig)
